c5dd7f5cac315ccb42aad3dd96b50047788337ae
salitha07 <Salithad889@gmail.com>
2026-06-10 22:37:12 +0530

    Add JWT authentication and protected routes

--- securescan/src/main/java/com/example/securescan/service/JwtService.java
+++ securescan/src/main/java/com/example/securescan/service/JwtService.java
@@ -1,32 +1,63 @@
 package com.example.securescan.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private final String SECRET_KEY =
-            "myVerySecretSecureScanKey123456";
+    private static final String SECRET =
+            "myVerySecretSecureScanKey123456myVerySecretSecureScanKey123456";
 
+    private final SecretKey key =
+            Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    // Generate JWT Token
     public String generateToken(String email) {
 
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(
-                        new Date(
-                                System.currentTimeMillis()
-                                        + 86400000
-                        )
-                )
-                .signWith(
-                        SignatureAlgorithm.HS256,
-                        SECRET_KEY
-                )
+                        new Date(System.currentTimeMillis() + 86400000)
+                ) // 24 hours
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    // Extract Email from Token
+    public String extractEmail(String token) {
+
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    // Validate Token
+    public boolean validateToken(String token) {
+
+        try {
+
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+
+            return true;
+
+        } catch (Exception e) {
+
+            return false;
+        }
     }
 }
